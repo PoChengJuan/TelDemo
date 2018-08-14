8df24f4d4ec61d0a9571b42863e91283950bbb1c
PoCheng <bigbangetw@gmail.com>
2018-08-14 23:46:47 +0800

    Initial Commit

---
+++ iKeaDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iKeaDemo
+//
+//  Created by PoCheng Juan on 2018/8/14.
+//  Copyright © 2018年 PoCheng Juan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
